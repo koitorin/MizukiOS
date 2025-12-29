4031e4472781fb50192c4bb94e6ff3378f989a47
Rin Koito <mouichidouryuuseigun@gmail.com>
2025-12-29 16:44:27 -0400

    should fix my mess
    Signed-off-by: Rin Koito <mouichidouryuuseigun@gmail.com>

---
+++ files/scripts/add_wants_niri.sh
@@ -0,0 +1,6 @@
+add_wants_niri() {
+    sed -i "s/\[Unit\]/\[Unit\]\nWants=$1/" "/usr/lib/systemd/user/niri.service"
+}
+add_wants_niri udiskie.service
+add_wants_niri foot-server.service
+cat /usr/lib/systemd/user/niri.service
