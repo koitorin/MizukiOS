43c6feb325301934ee84c9b52d796f5b1a4b4280
Rin Koito <mouichidouryuuseigun@gmail.com>
2025-12-29 21:34:21 -0400

    Keep multimedia enabled in nautilus script
    Signed-off-by: Rin Koito <mouichidouryuuseigun@gmail.com>

--- files/scripts/nautilus_thumbnails.sh
+++ files/scripts/nautilus_thumbnails.sh
@@ -2,6 +2,6 @@
 
 # Codecs for video thumbnails on nautilus
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
-dnf config-manager setopt fedora-multimedia.enabled=0
+dnf config-manager setopt fedora-multimedia.enabled=1
 dnf -y install --enablerepo=fedora-multimedia \
     ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
