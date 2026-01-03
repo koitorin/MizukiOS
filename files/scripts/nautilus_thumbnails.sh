988ffb4e4fb2086bcf33eda9fbe2663832119729
Rin Koito <mouichidouryuuseigun@gmail.com>
2026-01-03 06:25:52 -0400

    remove files that are provided by upstream
    Signed-off-by: Rin Koito <mouichidouryuuseigun@gmail.com>

--- files/scripts/nautilus_thumbnails.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Codecs for video thumbnails on nautilus
-dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
-dnf config-manager setopt fedora-multimedia.enabled=1
-dnf -y install --enablerepo=fedora-multimedia \
-    ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
